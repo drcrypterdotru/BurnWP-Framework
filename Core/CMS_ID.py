e6f3de1c53a8acf5d69b9329285cbe89fe811b72
DRCrypter RU <drcrypter1337@gmail.com>
2025-08-03 03:32:14 +0700

    Update CMS_ID
    + Keep collecting technologies for exploit later
    
    + Save different results txt

--- Core/CMS_ID.py
+++ Core/CMS_ID.py
@@ -54,50 +54,50 @@ class DetectCMS_Class:
                     FUC_SAVE.Saved_Result(r'DB_Results/Domain_Technology/Wordpress.txt', f'{RES_PROT['protocol']}\n')   
                     return RES_PROT['protocol'], 'WORDPRESS', WPNonce_code
                 
-                if self.MULTI_CMS:
+                elif self.MULTI_CMS: # Keep detecting multiple technologies on the same URL with different results
                     if 'joomla' in RAW_Source or 'index.php?option=' in RAW_Source \
                         or 'com_content' in RAW_Source or 'com_users' in RAW_Source \
                         or 'components/com_' in RAW_Source or ('mod_login' in RAW_Source and 'mod_menu' in RAW_Source):
                         FUC_SAVE.Saved_Result(r'DB_Results/Domain_Technology/Joomla.txt', f'{RES_PROT['protocol']}\n')
                         return RES_PROT['protocol'], 'JOOMLA', None
 
-                    if 'drupal' in RAW_Source or '/?q=' in RAW_Source or '/sites/default/' in RAW_Source or '/user/login' in RAW_Source:
+                    elif 'drupal' in RAW_Source or '/?q=' in RAW_Source or '/sites/default/' in RAW_Source or '/user/login' in RAW_Source:
                         FUC_SAVE.Saved_Result(r'DB_Results/Domain_Technology/Drupal.txt', f'{RES_PROT['protocol']}\n')
                         return RES_PROT['protocol'], 'DRUPAL', None
 
-                    if 'opencart' in RAW_Source or 'index.php?route=' in RAW_Source \
+                    elif 'opencart' in RAW_Source or 'index.php?route=' in RAW_Source \
                         or 'catalog/view/theme' in RAW_Source or 'controller/common' in RAW_Source or 'common/footer' in RAW_Source:
                         FUC_SAVE.Saved_Result(r'DB_Results/Domain_Technology/OpenCart.txt', f'{RES_PROT['protocol']}\n')
                         return RES_PROT['protocol'], 'OPENCART', None
 
-                    if 'magento' in RAW_Source or 'skin/frontend' in RAW_Source \
+                    elif 'magento' in RAW_Source or 'skin/frontend' in RAW_Source \
                         or 'static/frontend' in RAW_Source or 'checkout/cart' in RAW_Source or 'customer/account' in RAW_Source:
                         FUC_SAVE.Saved_Result(r'DB_Results/Domain_Technology/Magento.txt', f'{RES_PROT['protocol']}\n')
                         return RES_PROT['protocol'], 'MAGENTO', None 
                         
-                    if 'wordpress' in Header_RES or 'wp-' in Header_RES:
+                    elif 'wordpress' in Header_RES or 'wp-' in Header_RES:
                         FUC_SAVE.Saved_Result(r'DB_Results/Domain_Technology/Wordpress.txt', f'{RES_PROT['protocol']}\n')
                         return RES_PROT['protocol'], 'WORDPRESS', WPNonce_code 
                     
-                    if 'apache' in Header_RES or 'httpd' in Header_RES or 'mod_security' in Header_RES or 'mod_ssl' in Header_RES:
+                    elif 'apache' in Header_RES or 'httpd' in Header_RES or 'mod_security' in Header_RES or 'mod_ssl' in Header_RES:
                         FUC_SAVE.Saved_Result(r'DB_Results/Domain_Technology/Apache.txt', f'{RES_PROT['protocol']}\n')
                         return RES_PROT['protocol'], 'APACHE', None 
                     
 
-                    if 'nginx' in Header_RES:
+                    elif 'nginx' in Header_RES:
                         FUC_SAVE.Saved_Result(r'DB_Results/Domain_Technology/Nginx.txt', f'{RES_PROT['protocol']}\n')
                         return RES_PROT['protocol'], 'NGINX', None 
 
-                    if 'laravel' in Header_RES:
+                    elif 'laravel' in Header_RES:
                         FUC_SAVE.Saved_Result(r'DB_Results/Domain_Technology/Laravel.txt', f'{RES_PROT['protocol']}\n')
                         return RES_PROT['protocol'], 'LARAVEL', None 
 
-                    if 'php/' in Header_RES or '.php' in Header_RES:
+                    elif 'php/' in Header_RES or '.php' in Header_RES:
                         FUC_SAVE.Saved_Result(r'DB_Results/Domain_Technology/PHP.txt', f'{RES_PROT['protocol']}\n')
                         return RES_PROT['protocol'], 'PHP', None 
             
                 
-            if RES_PROT['protocol']:    
+            elif RES_PROT['protocol']:    
                 GET_XMLRPC = SmokeCore.Threaded_GET(
                                                     CFG_DOMAIN=RES_PROT['protocol'], 
                                                     CFG_VULN='xmlrpc.php', 
